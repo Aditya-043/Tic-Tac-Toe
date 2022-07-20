d6d2507fcab1bfc761b31110e1d6f2f5c26f07eb
Aditya Kumar <adityakumarraja043@gmail.com>
2022-07-20 12:23:51 +0530

    initial Commit

---
+++ app/src/main/java/com/adityakumarraja043gmail/tictactoe/MainActivity.kt
@@ -0,0 +1,213 @@
+package com.adityakumarraja043gmail.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputBinding
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import com.adityakumarraja043gmail.tictactoe.databinding.ActivityMainBinding
+import java.sql.ResultSet
+
+class MainActivity : AppCompatActivity() {
+
+    enum class Turn
+    {
+        NOUGHT,
+        CROSS
+    }
+
+    private var crossesScore = 0
+    private var noughtsScore = 0
+
+    private var firstTurn = Turn.CROSS
+    private var currentTurn = Turn.CROSS
+
+    private var boardList = mutableListOf<Button>()
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBoard()
+        binding.a1.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.a2.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.a3.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.b1.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.b2.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.b3.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.c1.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.c2.setOnClickListener {
+            boardTapped(it)
+        }
+        binding.c3.setOnClickListener {
+            boardTapped(it)
+        }
+
+    }
+
+    private fun initBoard() {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+
+
+
+    }
+
+
+    fun boardTapped(view: View) {
+
+        if(view !is Button)
+            return
+        addTOBoard(view)
+
+        if(checkForVictory(NOUGHT)){
+            noughtsScore++
+            result("Naught Wins")
+        }
+        else if(checkForVictory(CROSS)){
+            crossesScore++
+            result("Cross Wins")
+        }
+
+        if(fullBoard()){
+            result("Draw")
+        }
+    }
+
+    private fun checkForVictory(s: String): Boolean {
+        //Horizontally Victory
+        if(match(binding.a1,s) &&  match(binding.a2,s) && match(binding.a3,s))
+            return true
+        if(match(binding.b1,s) &&  match(binding.b2,s) && match(binding.b3,s))
+            return true
+        if(match(binding.c1,s) &&  match(binding.c2,s) && match(binding.c3,s))
+            return true
+
+
+        //Vertically Victory
+        if(match(binding.a1,s) &&  match(binding.b1,s) && match(binding.c1,s))
+            return true
+        if(match(binding.a2,s) &&  match(binding.b2,s) && match(binding.c2,s))
+            return true
+        if(match(binding.a3,s) &&  match(binding.b3,s) && match(binding.c3,s))
+            return true
+
+        //Diagonally Voctory
+        if(match(binding.a1,s) &&  match(binding.b2,s) && match(binding.c3,s))
+            return true
+        if(match(binding.a3,s) &&  match(binding.b2,s) && match(binding.c1,s))
+            return true
+
+
+        return false
+
+    }
+
+        private fun match(button: Button,symbol: String)  : Boolean = button.text == symbol
+
+
+    private fun result(title: String) {
+
+        var message = "\nNoughts $noughtsScore\n\n Crosses $crossesScore"
+            AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Reset"){
+                    _,_ ->
+                    resetBoard()
+                }
+
+                .setCancelable(false)
+                .show()
+
+
+    }
+
+    private fun resetBoard() {
+        for(button in boardList){
+            button.text = ""
+        }
+
+        if(firstTurn == Turn.NOUGHT)
+            firstTurn = Turn.CROSS
+
+        else if(firstTurn == Turn.CROSS)
+            firstTurn = Turn.NOUGHT
+
+        currentTurn = firstTurn
+        setTurnLevel()
+
+    }
+
+    private fun fullBoard(): Boolean {
+            for(button in boardList){
+                if(button.text == "")
+                    return false
+
+
+            }
+        return true
+
+    }
+
+    private fun addTOBoard(button: Button) {
+        if(button.text != "")
+            return
+
+        if(currentTurn== Turn.NOUGHT) {
+            button.text = "O"
+            currentTurn = Turn.CROSS
+        }
+
+        else if(currentTurn== Turn.CROSS) {
+            button.text = "X"
+            currentTurn = Turn.NOUGHT
+        }
+        setTurnLevel()
+
+
+
+    }
+
+    private fun setTurnLevel() {
+        var turnText = ""
+        if(currentTurn == Turn.CROSS)
+            turnText = "Turn $CROSS"
+
+        else if(currentTurn == Turn.NOUGHT)
+            turnText = "Turn $NOUGHT"
+
+        binding.turnTv.text = turnText
+    }
+
+    companion object{
+          const val NOUGHT = "O"
+            const val CROSS = "X"
+    }
+}
